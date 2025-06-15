72921e89f84076a8da8835184e3c5e34ea3ec71f
Chenglin Wang <csechenglin@ust.hk>
2025-06-15 17:55:15 +0100

    sync

--- rec_solver/core/solver.py
+++ rec_solver/core/solver.py
@@ -7,14 +7,14 @@ from .poly_expr import poly_expr_solving
 def solve_str(s):
     rec = parse_str(s)
     if isinstance(rec, LoopRecurrence):
-        # try:
-        res = solve_ultimately_periodic_symbolic(rec)
-        return res
-        # except:
-        #     res = poly_expr_solving(rec, 3)
-        #     if not res.is_trivial():
-        #         return res
-        #     return poly_expr_solving(rec, 3)
+        try:
+            res = solve_ultimately_periodic_symbolic(rec)
+            return res
+        except:
+            res = poly_expr_solving(rec, 3)
+            if not res.is_trivial():
+                return res
+            return poly_expr_solving(rec, 3)
     elif isinstance(rec, MultiRecurrence):
         return solve_multivariate_rec(rec)
     return None
